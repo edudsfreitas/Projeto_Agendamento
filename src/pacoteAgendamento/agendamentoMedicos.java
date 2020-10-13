c2a4cb59136a6e62b6e8f97ea05f12c1cdca586e
Eduardo Freitas <edu.sf98@hotmail.com>
2020-10-13 09:35:53 -0300

    v1.0

--- src/pacoteAgendamento/agendamentoMedicos.java
+++ src/pacoteAgendamento/agendamentoMedicos.java
@@ -19,53 +19,60 @@ public class agendamentoMedicos {
 		
 		System.out.println("Olá! Primeiramente, nos diga seu nome: ");
 		nome = ler.next();
+		
 		System.out.println("Agora, nos diga sua idade: ");
 		idade = ler.nextInt();
 		
 		if (idade <= 18 && idade >= 0)
-		{
+		{		
 			System.out.println("Médico disponível: Dr. Gonzales. \nEspecialidade: Pediatria. ");
-			for (int i=0; i<=1; i++){
+			
+			for (int i=0; i<=1; i++)
+			{
 				System.out.println("Horários disponíveis: " + horarios[i] + ":00 - Selecione: " + i + " para agendar");
-				
 			}
 			
 			hora = ler.nextInt();
 			
-			switch (hora) {
+			switch (hora) 
+			{
 				case 0: 
 						System.out.println("Obrigado " + nome + ", sua consulta foi agendada para as " + horarios[0] + ":00 com o Dr. Gonzales.");
 						break;
+						
 				case 1: 
-					System.out.println("Obrigado " + nome + ", sua consulta foi agendada para as " + horarios[1] + ":00 com o Dr. Gonzales.");
-					break;
+						System.out.println("Obrigado " + nome + ", sua consulta foi agendada para as " + horarios[1] + ":00 com o Dr. Gonzales.");
+						break;
+						
 				default: 
-					System.out.println("Dados inválidos!");
-		}
+						System.out.println("Dados inválidos!");
+			}
 		}
 		
 		else if (idade > 18 && idade <=120)
 		{
 			System.out.println("Médico disponível: Dra. Regina. \nEspecialidade: Clínico Geral. ");
+			
 			for (int i=2; i<=3; i++)
 			{
-				System.out.println("Horários disponíveis: " + horarios[i] + ":00 - Selecione: " + i + " para agendar");
-				
-						}
+				System.out.println("Horários disponíveis: " + horarios[i] + ":00 - Selecione: " + i + " para agendar");				
+			}
+			
 			hora = ler.nextInt();
 			
-			switch (hora) {
+			switch (hora) 
+			{
 				case 2: 
 						System.out.println("Obrigado " + nome + ", sua consulta foi agendada para as " + horarios[2] + ":00 com a Dra Regina.");
 						break;
 				case 3: 
-					System.out.println("Obrigado " + nome + ", sua consulta foi agendada para as " + horarios[3] + ":00 com a Dra Regina.");
-					break;
+						System.out.println("Obrigado " + nome + ", sua consulta foi agendada para as " + horarios[3] + ":00 com a Dra Regina.");
+						break;
 				default: 
-					System.out.println("Dados inválidos!");
-					
+						System.out.println("Dados inválidos!");					
 			}
 		}
+		
 		else 
 		{
 			System.out.println("Idade inválida ");
